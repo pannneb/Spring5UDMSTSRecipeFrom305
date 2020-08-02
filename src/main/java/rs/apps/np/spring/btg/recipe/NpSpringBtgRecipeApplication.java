fd5aa6c0ee15d43ac6c726bb2d93eb9a4bb90496
Nebojsa Pantic <panticnebojsa7788@gmail.com>
2020-08-02 22:33:17 +0200

    CircleCI test.

--- src/main/java/rs/apps/np/spring/btg/recipe/NpSpringBtgRecipeApplication.java
+++ src/main/java/rs/apps/np/spring/btg/recipe/NpSpringBtgRecipeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NpSpringBtgRecipeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NpSpringBtgRecipeApplication.class, args);
+		SpringApplicadsation.run(NpSpringBtgRecipeApplication.class, args);
 	}
 
 }
