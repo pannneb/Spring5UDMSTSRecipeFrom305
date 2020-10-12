c7aecdf980796d53654b9f30cb086902b7350dfd
Nebojsa Pantic <panticnebojsa7788@gmail.com>
2020-10-12 23:28:41 +0200

    Number Format Exception handling - 400error.html Closes #28

--- src/test/java/rs/apps/np/spring/btg/recipe/controllers/RecipeControllerTest.java
+++ src/test/java/rs/apps/np/spring/btg/recipe/controllers/RecipeControllerTest.java
@@ -64,6 +64,15 @@ public class RecipeControllerTest {
 	}
 
 	@Test
+	public void testGetRecipeNumberFormatExceptiond() throws Exception {
+		//when(recipeService.findById(anyLong()))
+		//		.thenThrow(rs.apps.np.spring.btg.recipe.exceptions.NotFoundException.class);
+		mockMvc.perform(get("/recipe/aaa/show"))
+			.andExpect(status().isBadRequest())
+			.andExpect(view().name("400error"));
+	}
+
+	@Test
 	public void testGetNewRecipeForm() throws Exception {
 		// RecipeCommand command = new RecipeCommand();
 
